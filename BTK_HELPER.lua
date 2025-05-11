914aedb7e26b296e11f5933b596941e2dfc32233
Ambatron132 <alwantira1@gmail.com>
2025-05-11 17:09:50 +0700

    Update BTK_HELPER.lua

--- BTK_HELPER.lua
+++ BTK_HELPER.lua
@@ -489,7 +489,7 @@ end
 
 
 LogToConsole("`9Script Will Run In `25 `9Seconds")
-SendPacket(2, "action|input\n|text|`0Proxy `6BTK `0By `#@Vermin `2ON!")
+SendPacket(2, "action|input\n|text|`0Proxy `6BTK `0By `#Osas Comunity `2ON!")
 Sleep(1000)
 
 
